761167bdcf6de6e73bdb47e4801822ff63c52fe2
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-05-13 13:24:11 +0400

    added puma support

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,17 +18,15 @@ nginx_site '000-default' do
 end
 
 include_recipe "unicorn"
+# include_recipe "puma"
 include_recipe "database"
 
 package "monit" do
   action :install
 end
 
-template "/etc/monit/conf.d/#{node['app_name']}" do
-  user "root"
-  owner "root"
-  source "unicorn.monitrc.erb"
-end
+include_recipe "unicorn::monit"
+# include_recipe "unicorn::puma"
 
 service "monit" do
   action [:enable, :start]
