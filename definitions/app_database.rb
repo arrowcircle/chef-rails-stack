c46d82ab7855877bb8151eeba66441614662a70a
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-07-29 14:13:53 +0400

    fixed mysql adapter name

--- definitions/app_database.rb
+++ definitions/app_database.rb
@@ -2,6 +2,10 @@ define :app_database do
   info = params[:info]
   config_path = params[:config_path]
   app_user = info['user']
+  db_type = case info['database']['type']
+  when 'mysql' then 'mysql2'
+  when 'postgresql' then 'postgresql'
+  end
 
   template "#{config_path}/database.yml" do
     user app_user
